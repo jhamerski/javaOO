a87acd7b9f203a31307183f9a0800ff61476f86b
Jonas Hamerski <jonashamerski87@gmail.com>
2020-10-26 23:44:19 -0300

    Classe aluno com método para calcular media.

--- TreinamentoJavaOO/src/br/com/classes/Aluno.java
+++ TreinamentoJavaOO/src/br/com/classes/Aluno.java
@@ -2,5 +2,37 @@ package br.com.classes;
 
 public class Aluno {
 
+	private String nome;
+	private double notaUm;
+	private double notaDois;
+	
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public double getNotaUm() {
+		return notaUm;
+	}
+
+	public void setNotaUm(double notaUm) {
+		this.notaUm = notaUm;
+	}
+
+	public double getNotaDois() {
+		return notaDois;
+	}
+
+	public void setNotaDois(double notaDois) {
+		this.notaDois = notaDois;
+	}
+
+	//metodo para calcular a media de notas dos alunos
+	public double calculaMedia() {
+		return (notaUm + notaDois) / 2;
+	}
 
 }
--- TreinamentoJavaOO/src/br/com/principal/Principal.java
+++ TreinamentoJavaOO/src/br/com/principal/Principal.java
@@ -1,9 +1,30 @@
 package br.com.principal;
 
+import java.util.Scanner;
+
+import br.com.classes.Aluno;
+
 public class Principal {
-
+	
 	public static void main(String[] args) {
+		
+		Scanner teclado = new Scanner(System.in);
 
+		Aluno aluno = new Aluno();
+		
+		System.out.print("Informe o nome do aluno: ");
+		String nome = teclado.next();
+		aluno.setNome(nome);
+		
+		System.out.print("Informe primeira nota: ");
+		double notaUm = teclado.nextDouble();
+		aluno.setNotaUm(notaUm);
+		
+		System.out.print("Informe segunda nota: ");
+		double notaDois = teclado.nextDouble();
+		aluno.setNotaDois(notaDois);
+
+		System.out.println("O aluno " + aluno.getNome() + " ficou com média " + aluno.calculaMedia());
 		
 	}
 
