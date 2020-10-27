d607bb5cd319025e9f3a8ac45b6447d01800c60f
Jonas Hamerski <jonashamerski87@gmail.com>
2020-10-26 23:15:44 -0300

    Removendo os códigos - Refazer o programa.

--- TreinamentoJavaOO/src/br/com/constantes/StatusAluno.java
+++ TreinamentoJavaOO/src/br/com/constantes/StatusAluno.java
@@ -1,7 +1,5 @@
 package br.com.constantes;
 
 public class StatusAluno {
-	public static String APROVADO = "Aprovado";
-	public static String REPROVADO = "Reprovado";
-	public static String RECUPERACAO= "Recuperação";
+
 }
