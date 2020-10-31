68c8d5bc01d64ccd84a43418c3f5cee97b5d67a0
Jonas Hamerski <jonashamerski87@gmail.com>
2020-10-31 01:13:40 -0300

    Polimorfismo

--- TreinamentoJavaOO/src/br/com/principal/TestandoClasseFilha.java
+++ TreinamentoJavaOO/src/br/com/principal/TestandoClasseFilha.java
@@ -2,6 +2,7 @@ package br.com.principal;
 
 import br.com.classes.Aluno;
 import br.com.classes.Diretor;
+import br.com.classes.Pessoa;
 import br.com.classes.Secretario;
 
 public class TestandoClasseFilha {
@@ -13,10 +14,12 @@ public class TestandoClasseFilha {
 		aluno.setIdade(16);
 
 		Diretor diretor = new Diretor();
+		diretor.setNome("Patryck");
 		diretor.setRegitroEducacao("7263716278316");
 		diretor.setIdade(50);
 		
 		Secretario secretario = new Secretario();
+		secretario.setNome("Arthur");
 		secretario.setExperiencia("Ciência da Computação");
 		secretario.setIdade(18);
 		
@@ -31,6 +34,14 @@ public class TestandoClasseFilha {
 		System.out.println("Aluno tem salário de " + aluno.salario());
 		System.out.println("Ditero tem salário de " + diretor.salario());
 		System.out.println("Secretário tem salário de " + secretario.salario());
-
+		
+		teste(aluno);
+		teste(diretor);
+		teste(secretario);
+		
+	}
+	
+	public static void teste(Pessoa pessoa) {
+		System.out.println("Esta pessoa é demais = " + pessoa.getNome() + " e o salário é de: " + pessoa.salario());
 	}
 }
