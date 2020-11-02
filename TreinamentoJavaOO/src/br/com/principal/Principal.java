0299040ed3dbd609baa5efd23dcb02c3c1a16b7d
Jonas Hamerski <jonashamerski87@gmail.com>
2020-11-02 03:47:04 -0200

    Entendendo a classe Exception

--- TreinamentoJavaOO/src/br/com/principal/Principal.java
+++ TreinamentoJavaOO/src/br/com/principal/Principal.java
@@ -17,101 +17,116 @@ import br.com.interfaces.PermitirAcesso;
 public class Principal {
 
 	public static void main(String[] args) {
-		
+
 		try {
-		Scanner teclado = new Scanner(System.in);
+			Scanner teclado = new Scanner(System.in);
 
-		System.out.print("Informe o login: ");
-		String login = teclado.next();
-		System.out.print("Informe e senha: ");
-		String senha = teclado.next();
+			System.out.print("Informe o login: ");
+			String login = teclado.next();
+			System.out.print("Informe e senha: ");
+			String senha = teclado.next();
 
-		// FuncaoAutenticacao autenticacao = new FuncaoAutenticacao();
+			// FuncaoAutenticacao autenticacao = new FuncaoAutenticacao();
 
-		// PermitirAcesso permitirAcesso = new Secretario(login, senha);
+			// PermitirAcesso permitirAcesso = new Secretario(login, senha);
 
-		if (new FuncaoAutenticacao(new Diretor(login, senha))
-				.autenticar()) {/* Vou travar o contrato para autorizar o login tendo contrato 100% legítimo */
+			if (new FuncaoAutenticacao(new Diretor(login, senha))
+					.autenticar()) {/* Vou travar o contrato para autorizar o login tendo contrato 100% legítimo */
 
-			List<Aluno> alunos = new ArrayList<Aluno>();
+				List<Aluno> alunos = new ArrayList<Aluno>();
 
-			// lista que dentro dela temos uma chave que identifica uma sequencia de valores
-			HashMap<String, List<Aluno>> maps = new HashMap<String, List<Aluno>>();
+				// lista que dentro dela temos uma chave que identifica uma sequencia de valores
+				HashMap<String, List<Aluno>> maps = new HashMap<String, List<Aluno>>();
 
-			System.out.print("Quantos alunos deseja cadastrar: ");
-			int qtd = teclado.nextInt();
+				System.out.print("Quantos alunos deseja cadastrar: ");
+				int qtd = teclado.nextInt();
 
-			for (int i = 1; i <= qtd; i++) {
+				for (int i = 1; i <= qtd; i++) {
 
-				Aluno aluno = null;
+					Aluno aluno = null;
 
-				System.out.print("Informe o nome do " + i + "° aluno: ");
-				String nome = teclado.next();
-				aluno.setNome(nome);
+					System.out.print("Informe o nome do " + i + "° aluno: ");
+					String nome = teclado.next();
+					aluno.setNome(nome);
 
-				for (int j = 1; j <= 2; j++) {
-					Disciplina disciplina = new Disciplina();
+					for (int j = 1; j <= 2; j++) {
+						Disciplina disciplina = new Disciplina();
 
-					System.out.print("Informe o nome da " + j + "° disciplina: ");
-					String nomeDisciplina = teclado.next();
-					System.out.print("Informe a nota " + j + "° nota: ");
-					double nota = teclado.nextDouble();
+						System.out.print("Informe o nome da " + j + "° disciplina: ");
+						String nomeDisciplina = teclado.next();
+						System.out.print("Informe a nota " + j + "° nota: ");
+						double nota = teclado.nextDouble();
 
-					disciplina.setDisciplina(nomeDisciplina);
-					disciplina.setNota(nota);
+						disciplina.setDisciplina(nomeDisciplina);
+						disciplina.setNota(nota);
 
-					aluno.getDisciplinas().add(disciplina);
+						aluno.getDisciplinas().add(disciplina);
+					}
+
+					alunos.add(aluno);
+
 				}
 
-				alunos.add(aluno);
-
-			}
-
-			for (Aluno alu : alunos) {
-				System.out.println("O aluno " + alu.getNome() + " ficou com média " + alu.calculaMedia() + ".");
-			}
-
-			System.out.print("Informe o nome do aluno para saber se foi aprovado: ");
-			String procurado = teclado.next();
-			for (Aluno aluno : alunos) {
-				if (aluno.getNome().equalsIgnoreCase(procurado)) {
-					System.out.println("Aluno: " + aluno.getNome().toUpperCase() + " está " + aluno.statusAprovacao());
+				for (Aluno alu : alunos) {
+					System.out.println("O aluno " + alu.getNome() + " ficou com média " + alu.calculaMedia() + ".");
 				}
-			}
 
-			maps.put(StatusAluno.APROVADO, new ArrayList<Aluno>());
-			maps.put(StatusAluno.RECUPERACAO, new ArrayList<Aluno>());
-			maps.put(StatusAluno.REPROVADO, new ArrayList<Aluno>());
-
-			for (Aluno aluno : alunos) {
-				if (aluno.statusAprovacao().equalsIgnoreCase(StatusAluno.APROVADO)) {
-					maps.get(StatusAluno.APROVADO).add(aluno);
-				} else if (aluno.statusAprovacao().equalsIgnoreCase(StatusAluno.RECUPERACAO)) {
-					maps.get(StatusAluno.RECUPERACAO).add(aluno);
-				} else if (aluno.statusAprovacao().equalsIgnoreCase(StatusAluno.REPROVADO)) {
-					maps.get(StatusAluno.REPROVADO).add(aluno);
+				System.out.print("Informe o nome do aluno para saber se foi aprovado: ");
+				String procurado = teclado.next();
+				for (Aluno aluno : alunos) {
+					if (aluno.getNome().equalsIgnoreCase(procurado)) {
+						System.out.println(
+								"Aluno: " + aluno.getNome().toUpperCase() + " está " + aluno.statusAprovacao());
+					}
 				}
+
+				maps.put(StatusAluno.APROVADO, new ArrayList<Aluno>());
+				maps.put(StatusAluno.RECUPERACAO, new ArrayList<Aluno>());
+				maps.put(StatusAluno.REPROVADO, new ArrayList<Aluno>());
+
+				for (Aluno aluno : alunos) {
+					if (aluno.statusAprovacao().equalsIgnoreCase(StatusAluno.APROVADO)) {
+						maps.get(StatusAluno.APROVADO).add(aluno);
+					} else if (aluno.statusAprovacao().equalsIgnoreCase(StatusAluno.RECUPERACAO)) {
+						maps.get(StatusAluno.RECUPERACAO).add(aluno);
+					} else if (aluno.statusAprovacao().equalsIgnoreCase(StatusAluno.REPROVADO)) {
+						maps.get(StatusAluno.REPROVADO).add(aluno);
+					}
+				}
+
+				System.out.println("\n--------LISTA DOS APROVADOS--------");
+				for (Aluno aluno : maps.get(StatusAluno.APROVADO)) {
+					System.out.println("Aluno " + aluno.getNome() + " ficou com média: " + aluno.calculaMedia());
+				}
+				System.out.println("-------LISTA DOS RECUPERAÇÃO-------");
+				for (Aluno aluno : maps.get(StatusAluno.RECUPERACAO)) {
+					System.out.println("Aluno " + aluno.getNome() + " ficou com média: " + aluno.calculaMedia());
+				}
+				System.out.println("--------LISTA DOS REPROVADOS-------");
+				for (Aluno aluno : maps.get(StatusAluno.REPROVADO)) {
+					System.out.println("Aluno " + aluno.getNome() + " ficou com média: " + aluno.calculaMedia());
+				}
+			} else {
+				System.out.println("Login ou senha invádos.");
 			}
 
-			System.out.println("\n--------LISTA DOS APROVADOS--------");
-			for (Aluno aluno : maps.get(StatusAluno.APROVADO)) {
-				System.out.println("Aluno " + aluno.getNome() + " ficou com média: " + aluno.calculaMedia());
-			}
-			System.out.println("-------LISTA DOS RECUPERAÇÃO-------");
-			for (Aluno aluno : maps.get(StatusAluno.RECUPERACAO)) {
-				System.out.println("Aluno " + aluno.getNome() + " ficou com média: " + aluno.calculaMedia());
-			}
-			System.out.println("--------LISTA DOS REPROVADOS-------");
-			for (Aluno aluno : maps.get(StatusAluno.REPROVADO)) {
-				System.out.println("Aluno " + aluno.getNome() + " ficou com média: " + aluno.calculaMedia());
-			}
-		} else {
-			System.out.println("Login ou senha invádos.");
-		}
-		
-	} catch (Exception e) {
+		} catch (Exception e) {
+
+			StringBuilder saida = new StringBuilder();
+
+			/* Imprime erro no consolel java */
 			e.printStackTrace();
+
+			/* Mensagem do erro ou causa */
+			System.out.println("Mensagem: " + e.getMessage());
+
 			System.out.println("Erro ao processar notas.");
+
+			for (int i = 0; i < e.getStackTrace().length; i++) {
+				System.out.println("Classe de erro: " + e.getStackTrace()[i].getClassName());
+				System.out.println("Método de erro: " + e.getStackTrace()[i].getMethodName());
+				System.out.println("Linha de erro: " + e.getStackTrace()[i].getLineNumber());
+			}
 		}
 	}
 
