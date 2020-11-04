ee59e0074019eef27535c1e601759117a5092b40
Jonas Hamerski <jonashamerski87@gmail.com>
2020-11-03 23:08:09 -0200

    Array de forma dinâmica.

--- TreinamentoJavaOO/src/br/com/principal/ArrayVetor.java
+++ TreinamentoJavaOO/src/br/com/principal/ArrayVetor.java
@@ -1,17 +1,27 @@
 package br.com.principal;
 
+import java.util.Scanner;
+
 public class ArrayVetor {
 
 	public static void main(String[] args) {
 		
-		double notas[] = new double[4];
-		double[] notas1 = new double[5];
+		Scanner teclado = new Scanner(System.in);
+		System.out.print("Quanto posições tem o Array? ");
+		int posicoes = teclado.nextInt();
+		
+		double notas[] = new double[posicoes];
+		//double[] notas1 = new double[5];
 		
 		System.out.println();
 		for (int i = 0; i < notas.length; i++) {
-			notas[i] = i;
+			System.out.print("Informe o " + (i + 1) + "° valor: ");
+			notas[i] = teclado.nextDouble();
 		}
 		
-		System.out.println();
+		System.out.println("Resultado: ");
+		for (int i = 0; i < notas.length; i++) {
+			System.out.print(notas[i] + " - ");
+		}
 	}
 }
