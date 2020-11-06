14379987649a4a77e8cc4b824e76b6c49e65a9ab
Jonas Hamerski <jonashamerski87@gmail.com>
2020-11-06 00:29:02 -0200

    Thread em objeto com Runnable

--- TreinamentoJavaOO/src/br/com/thread/AulaThread.java
+++ TreinamentoJavaOO/src/br/com/thread/AulaThread.java
@@ -6,33 +6,58 @@ public class AulaThread {
 
 	public static void main(String[] args) throws InterruptedException {
 
-		/*Thread processando em paralelo*/
-		new Thread() {
-			public void run() {/* Executa o que nós queremos */
-				/* Código da Rotina */
+		Thread threadEmail = new Thread(thread1);
+		threadEmail.start();
 
-				/* Código da rotina que for executar em paralelo */
-				for (int i = 0; i < 10; i++) {
-					System.out.println("Executando alguma rotina. Exemplo: Envio de e-mail...");
+		// ###################################DIVISAO DAS THREAD
 
-					/* Executar o envio com um determinado intervalo de tempo */
-					try {
-						Thread.sleep(1000);
-					} catch (InterruptedException e) {
-						e.printStackTrace();
-					} /* Dá um tempo */
-				}
-				/* Fim código paralelo */
-			};
-		}.start();/* Liga a thread que fica processando paralelamente */
-
-		/*
-		 * Código do sistama para o usuário chegou ao fim, ou continua o fluxo do
-		 * programa
-		 */
-		System.out.println("CHEGOU AO FIM O CÓDIGO DE THREAD");
+		Thread threadNotaFiscal = new Thread(thread2);
+		threadNotaFiscal.start();
 
 		/* Fluxo do sistema tem cotinuidade... */
 		JOptionPane.showMessageDialog(null, "Sistema continua executando para o usuário...");
 	}
+
+	private static Runnable thread1 = new Runnable() {
+
+		@Override
+		public void run() {
+			/* Código da Rotina */
+
+			/* Código da rotina que for executar em paralelo */
+			for (int i = 0; i < 10; i++) {
+				System.out.println("Executando alguma rotina. Exemplo: Envio de e-mail...");
+
+				/* Executar o envio com um determinado intervalo de tempo */
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				} /* Dá um tempo */
+			}
+			/* Fim código paralelo */
+		}
+	};
+
+	private static Runnable thread2 = new Runnable() {
+
+		@Override
+		public void run() {
+			/* Código da Rotina */
+
+			/* Código da rotina que for executar em paralelo */
+			for (int i = 0; i < 10; i++) {
+				System.out.println("Executando alguma rotina. Exemplo: Envio de Nota Fiscal...");
+
+				/* Executar o envio com um determinado intervalo de tempo */
+				try {
+					Thread.sleep(3000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				} /* Dá um tempo */
+			}
+			/* Fim código paralelo */
+		}
+	};
+
 }
