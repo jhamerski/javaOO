b6062f04ab45ac0fed0a7b6ec9754a0a05ed54a6
Jonas Hamerski <jonashamerski87@gmail.com>
2020-11-05 22:27:19 -0200

    Conversao Array para Lista e Lista para Array

--- TreinamentoJavaOO/src/br/com/principal/SplitArray.java
+++ TreinamentoJavaOO/src/br/com/principal/SplitArray.java
@@ -1,18 +1,29 @@
 package br.com.principal;
 
-public class SplitArray {
-	
-	public static void main(String[] args) {
-		
-		String text = " Jonas Hamerski, curso java, 80, 70, 90, 89";
-		
-		String[] valoresArray = text.split(",");
+import java.util.Arrays;
+import java.util.List;
 
-		System.out.println(valoresArray);
-		
+public class SplitArray {
+
+	public static void main(String[] args) {
+
+		String text = " Jonas Hamerski, curso java, 80, 70, 90, 89";
+
+		String[] valoresArray = text.split(",");
 		for (int i = 0; i < valoresArray.length; i++) {
 			System.out.println(valoresArray[i]);
 		}
+
+		List<String> list = Arrays.asList(valoresArray);
+		for (String txt : list) {
+			System.out.println(txt);
+		}
+
+		String[] conversaoArray = list.toArray(new String[6]);
+		for (int i = 0; i < conversaoArray.length; i++) {
+			System.out.println(conversaoArray[i]);
+		}
+
 	}
 
 }
