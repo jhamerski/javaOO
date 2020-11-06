1281205ea05e694dee6c32b603ecf28e45a19572
Jonas Hamerski <jonashamerski87@gmail.com>
2020-11-05 23:37:07 -0200

    Primeira Thread

--- TreinamentoJavaOO/src/br/com/thread/AulaThread.java
+++ TreinamentoJavaOO/src/br/com/thread/AulaThread.java
@@ -1,17 +1,38 @@
 package br.com.thread;
 
+import javax.swing.JOptionPane;
+
 public class AulaThread {
 
 	public static void main(String[] args) throws InterruptedException {
 
-		for (int i = 0; i < 10; i++) {
-			System.out.println("Executando alguma rotina. Exemplo: Envio de e-mail...");
+		/*Thread processando em paralelo*/
+		new Thread() {
+			public void run() {/* Executa o que nós queremos */
+				/* Código da Rotina */
 
-			/* Executar o envio com um determinado intervalo de tempo */
-			Thread.sleep(1000);/*Dá um tempo*/
-		}
-		
+				/* Código da rotina que for executar em paralelo */
+				for (int i = 0; i < 10; i++) {
+					System.out.println("Executando alguma rotina. Exemplo: Envio de e-mail...");
+
+					/* Executar o envio com um determinado intervalo de tempo */
+					try {
+						Thread.sleep(1000);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					} /* Dá um tempo */
+				}
+				/* Fim código paralelo */
+			};
+		}.start();/* Liga a thread que fica processando paralelamente */
+
+		/*
+		 * Código do sistama para o usuário chegou ao fim, ou continua o fluxo do
+		 * programa
+		 */
 		System.out.println("CHEGOU AO FIM O CÓDIGO DE THREAD");
 
+		/* Fluxo do sistema tem cotinuidade... */
+		JOptionPane.showMessageDialog(null, "Sistema continua executando para o usuário...");
 	}
 }
