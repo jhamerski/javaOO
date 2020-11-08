1fde2d65cfe6dfcf479ea900e41c031ca80226e5
Jonas Hamerski <jonashamerski87@gmail.com>
2020-11-08 13:26:19 -0200

    Conhecendo o GridBagConstraints e posicionamento de Layout

--- TreinamentoJavaOO/src/br/com/thread/TelaTimeThread.java
+++ TreinamentoJavaOO/src/br/com/thread/TelaTimeThread.java
@@ -1,17 +1,57 @@
 package br.com.thread;
 
+import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 
 import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
 
 public class TelaTimeThread extends JDialog {
 
+	/*Painel de Componentes*/
+	private JPanel jPanel = new JPanel(new GridBagLayout());
+	private JLabel descricaoHora = new JLabel("Time da THREAD1");	
+	private JTextField mostraTempo = new JTextField();
+	
+	private JLabel descricaoHora2 = new JLabel("Time da THREAD2");	
+	private JTextField mostraTempo2 = new JTextField();
+	
 	public TelaTimeThread() {
 		setTitle("Minha tela de time com THREAD");
 		setSize(new Dimension(240, 240));
 		setLocationRelativeTo(null);
 		setResizable(false);
+
+		/*Controlador de posicionamento de componentes*/
+		GridBagConstraints gridBagConstraints = new GridBagConstraints();
+
+		gridBagConstraints.gridx = 0;
+		gridBagConstraints.gridy = 0;
 		
+		descricaoHora.setPreferredSize(new Dimension(200, 25));
+		jPanel.add(descricaoHora, gridBagConstraints);
+		
+		mostraTempo.setPreferredSize(new Dimension(200, 25));
+		gridBagConstraints.gridy++;
+		jPanel.add(mostraTempo, gridBagConstraints);
+		
+		
+		descricaoHora2.setPreferredSize(new Dimension(200, 25));
+		gridBagConstraints.gridy++;
+		jPanel.add(descricaoHora2, gridBagConstraints);
+		
+		mostraTempo2.setPreferredSize(new Dimension(200, 25));
+		gridBagConstraints.gridy++;
+		jPanel.add(mostraTempo2, gridBagConstraints);
+		
+		
+
+		add(jPanel, BorderLayout.WEST);
 		setVisible(true);
 	}
 }
